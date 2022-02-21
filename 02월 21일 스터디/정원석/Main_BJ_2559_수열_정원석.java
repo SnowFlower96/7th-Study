49fcae3dbc9d7dc5b43d9289e1362c5132ff07ae
Jeong-Wonseok <jws4858@gmail.com>
2022-02-21 20:39:55 +0900

    정원석

---
+++ 02월 21일 스터디/정원석/Main_BJ_2559_수열_정원석.java
@@ -0,0 +1,59 @@
+package com.ssafy.BJ01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_2559_수열_정원석 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int term = Integer.parseInt(st.nextToken());
+		
+		int[] seq = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+//		배열 사용 //이상하게 계속 틀림 왜 틀린지 모르겠음
+		// 40퍼 에서 오류뜸...
+//		int sum_max = -101;
+//		int sum_max = Integer.MIN_VALUE; // 이걸 사용하니까 맞았다. 이유가 무엇일까??
+		// 문제에서 주어진 최소값은 -100 내가 설정한 최소값은 -101... 머야???? 이유를 모르겠네...
+//		
+//		for(int i = 0; i <= N - term; i++) {
+//			int sum = 0;
+//			for(int j = i; j < i + term; j++) {
+//				sum += seq[j];
+//			}
+//			sum_max = Math.max(sum_max, sum);
+//		}
+//		
+//		System.out.println(sum_max);	
+		
+		//투포인터
+		//밑의 코드를 활용하면 시간단축기 가능하다.
+		// 1 2 3 4 5 6 7 8 9 이 있고 term이 4라면
+		// 1 2 3 4		     를 처음에 저장을 하고
+		//   2 3 4 5	     로 한칸 전진하는 경우 1을 저장한 변수에서 뺴고 새로 들어오는 5를 더해준다.
+		int sum = 0; 
+		for(int i = 0; i < term; i++) {
+			sum += seq[i];
+		}
+		int sum_max = sum;
+		for(int i = term; i< N; i++) {
+			sum = sum - seq[i-term] + seq[i]; 
+			sum_max = Math.max(sum_max,  sum);
+		}
+		
+		System.out.println(sum_max);
+	}
+
+}
